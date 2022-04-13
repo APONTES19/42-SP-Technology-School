1f4ce4bbd09404aebb11b325cddb01651642f4b1
Lucas Martins <apontes19@gmail.com>
2022-04-13 17:20:26 -0300

    base pipex

---
+++ Projetos_42/pipex/headers/libs.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libs.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucasmar < lucasmar@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 15:00:22 by lucasmar          #+#    #+#             */
+/*   Updated: 2022/04/13 15:03:09 by lucasmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBS_H
+# define LIBS_H
+
+# include <stdio.h>
+# include "../libs/ft_printf/headers/ft_printf.h"
+# include "../libs/libft/headers/libft.h"
+
+#endif
